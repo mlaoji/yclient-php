ea6071351733c18ed83803e225877301b542dd02
yangjiguang <yangjiguang@bigeyegroup.com>
2021-02-22 12:06:33 +0800

    更新日志方法

--- example.php
+++ example.php
@@ -1,9 +1,17 @@
 <?php
 require dirname(__FILE__).'/YClient.php';
-$c = YClient::getInstance("127.0.0.1:9002", "username","password");
+$c = YClient::getInstance("cmn_passport");
 //$c->setLogFile("logs/access.log");
 try {
-    $s = $c->request("passport/getUserInfo", array("uid"=>"1"));
+    $s = $c->request("passport/getUsersInfo", array("name"=>"user","uid"=>1,"field"=>"a"));
+    var_dump($s);
+    exit;
+    //$s = $c->request("counter/setCounter", array("name"=>"user","rid"=>1,"field"=>"a","num"=>1));
+    $s = $c->request("counter/getCounter", array("name"=>"user","rid"=>1,"field"=>"a"));
+    var_dump($s);
+    $s = $c->request("counter/subCounter", array("name"=>"user","rid"=>1,"field"=>"a","num"=>1));
+    var_dump($s);
+    $s = $c->request("counter/getCounter", array("name"=>"user","rid"=>1,"field"=>"a"));
     var_dump($s);
 } catch(YClientException $e) {
     echo "error: [". $e->getCode() . "] ". $e->getMessage();
--- lib/YLogger.php
+++ lib/YLogger.php
@@ -2,15 +2,15 @@
 class YLogger
 {
     const LOG_LEVEL_NONE    = 0x00;
-    const LOG_LEVEL_ACCESS  = 0x01;
-    const LOG_LEVEL_FATAL   = 0x02;
-    const LOG_LEVEL_WARN    = 0x04;
-    const LOG_LEVEL_NOTICE  = 0x08;
+    const LOG_LEVEL_ERROR   = 0x01;
+    const LOG_LEVEL_WARN    = 0x02;
+    const LOG_LEVEL_ACCESS  = 0x04;
+    const LOG_LEVEL_INFO    = 0x08;
     const LOG_LEVEL_DEBUG   = 0x10;
     const LOG_LEVEL_ALL     = 0xFF;
     
     const LOG_SPLIT_NONE = 0;
-    const LOG_SPLIT_DAY = 1;
+    const LOG_SPLIT_DAY  = 1;
     const LOG_SPLIT_HOUR = 2;
 
     /**
@@ -18,10 +18,10 @@ class YLogger
      */
     public static $levels = array(
         self::LOG_LEVEL_NONE    => 'NONE',
-        self::LOG_LEVEL_ACCESS  => 'ACCESS',
-        self::LOG_LEVEL_FATAL   => 'FATAL',
+        self::LOG_LEVEL_ERROR   => 'ERROR',
         self::LOG_LEVEL_WARN    => 'WARN',
-        self::LOG_LEVEL_NOTICE  => 'NOTICE',
+        self::LOG_LEVEL_ACCESS  => 'ACCESS',
+        self::LOG_LEVEL_INFO    => 'INFO',
         self::LOG_LEVEL_DEBUG   => 'DEBUG',
         self::LOG_LEVEL_ALL     => 'ALL',
     );
@@ -31,6 +31,8 @@ class YLogger
     protected $log_split;
 
     protected $log_file;
+
+    protected $log_path;
 
     protected $logid;
 
@@ -44,7 +46,12 @@ class YLogger
         $this->level      = isset(YConfig::$log_conf['level']) ? (int)YConfig::$log_conf['level'] : 0x07; //日志级别
         $this->log_split  = isset(YConfig::$log_conf['split']) ? (int)YConfig::$log_conf['split'] : self::LOG_SPLIT_DAY; //日志切分
         $this->log_file   = isset(YConfig::$log_conf['logfile']) ? YConfig::$log_conf['logfile'] : dirname(__DIR__) . '/logs/yclient.log'; //日志文件
-        $this->other_log_files = YConfig::$log_conf['others'];   //其他日志文件
+        $this->log_path   = dirname($this->log_file); //默认日志目录
+        $this->other_log_files = isset(YConfig::$log_conf['logfile']) ? YConfig::$log_conf['others'] : null;   //其他日志文件
+
+        if(!is_dir($this->log_path)) {
+            mkdir($this->log_path, 0777, true);
+        }
     }/*}}}*/
 
     public static function getInstance() {/*{{{*/
@@ -60,9 +67,9 @@ class YLogger
         return $instance->writeLog(self::LOG_LEVEL_DEBUG, $errlog, $errorno, $args, $depth + 1);
     }/*}}}*/
 
-    public static function notice($errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
+    public static function info($errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
         $instance = self::getInstance();
-        return $instance->writeLog(self::LOG_LEVEL_NOTICE, $errlog, $errorno, $args, $depth + 1);
+        return $instance->writeLog(self::LOG_LEVEL_INFO, $errlog, $errorno, $args, $depth + 1);
     }/*}}}*/
 
     public static function warn($errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
@@ -70,9 +77,9 @@ class YLogger
         return $instance->writeLog(self::LOG_LEVEL_WARN, $errlog, $errorno, $args, $depth + 1);
     }/*}}}*/
 
-    public static function fatal($errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
+    public static function error($errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
         $instance = self::getInstance();
-        return $instance->writeLog(self::LOG_LEVEL_FATAL, $errlog, $errorno, $args, $depth + 1);
+        return $instance->writeLog(self::LOG_LEVEL_ERROR, $errlog, $errorno, $args, $depth + 1);
     }/*}}}*/
 
     public static function access($errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
@@ -88,7 +95,7 @@ class YLogger
     public static function getClientIP() {/*{{{*/
         $ip = "";
         if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != '127.0.0.1') {
-            $ips = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
             $pos = strpos($ip, ',');
             if ($pos > 0) {
@@ -118,12 +125,12 @@ class YLogger
     }/*}}}*/
 
     public function writeLog($level, $errlog, $errorno = 0, $args = null, $depth = 0) {/*{{{*/
-        if ($level > $this->level || !isset(self::$levels[$level])) {
+        if (($level&$this->level) == 0) {
             return;
         }
 
         $level_name    = self::$levels[$level];
-        $log_file = $this->getLogFile($level);
+        $log_file = $this->getLogFile($level_name);
 
         $debug_trace = debug_backtrace();
         if ($depth >= count($debug_trace)) {
@@ -142,7 +149,7 @@ class YLogger
         $time_used = microtime(true)*1000 - $this->start_time;
 
         $str = sprintf(
-            "%s: %s [%s:%d] errno[%d] ip[%s] logId[%u] uri[%s] time_used[%d] %s%s\n",
+            "%s:%s [%s:%d] errno[%d] ip[%s] logId[%u] uri[%s] time_used[%d] %s%s\n",
             $level_name,
             date('m-d H:i:s:', time()),
             $filename, 
@@ -166,29 +173,33 @@ class YLogger
             return;
         }
 		
-        $args = '';
+        $args_val = '';
         if( is_array($args) && count($args) > 0) {
-            foreach($args as $key => $value) {
-                $args .= $key . "[$value] ";
+            foreach($args as $k => $value) {
+                $args_val .= $k . "[$value] ";
             }
         }
 
         $str = sprintf( "%s: time[%s] ip[%s] logId[%u] uri[%s] %s%s\n",
-                        $key,
-                        date('m-d H:i:s:', time()),
-                        self::getClientIP(),
-                        $this->logid,
-                        isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '',
-                        $args, $errlog);
+            $key,
+            date('m-d H:i:s:', time()),
+            self::getClientIP(),
+            $this->logid,
+            isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '',
+            $args_val,
+            $errlog
+        );
 
         return file_put_contents($log_file, $str, FILE_APPEND);
     }/*}}}*/
 
-    private function getLogFile($level) {/*{{{*/
+    private function getLogFile($level_name) {/*{{{*/
         $log_file = $this->log_file;
-        if (($level & self::LOG_LEVEL_WARN) || ($level & self::LOG_LEVEL_FATAL)) {
-            $log_file .= '.wf';
+
+        if ($level_name != "ACCESS") {
+            $log_file .= '.' . strtolower($level_name);
         }
+
         if ($this->log_split == self::LOG_SPLIT_DAY) {
             $log_file .= '.' . date("Ymd");
         } elseif ($this->log_split == self::LOG_SPLIT_HOUR) {
@@ -199,11 +210,11 @@ class YLogger
     }/*}}}*/
 
     private function getOtherLogFile($key) {/*{{{*/
-        if(!isset($this->other_log_files[$key])) {
-			return false;
+        if(isset($this->other_log_files[$key])) {
+            $log_file = $this->other_log_files[$key];
+        } else {
+            $log_file = $this->log_path . "/" . $key . ".log";
         }
-			
-        $log_file = $this->other_log_files[$key];
 
 		if ($this->log_split == self::LOG_SPLIT_DAY) {
             $log_file .= '.' . date("Ymd");
